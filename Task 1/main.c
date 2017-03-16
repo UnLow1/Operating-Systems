e7f3631abef87425e75058dc8a09eece521ba292
Adam Jamka <adam.jamka.1995@gmail.com>
2017-03-16 02:22:53 +0100

    Update main.c

--- Task 1/main.c
+++ Task 1/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "binTree.h"
 #include "list.h"
 
 
 int main() {
-//    Node *head = NULL;
-    TreeNode *root = NULL;
-    FILE *file = fopen("dane.txt", "r");
+    Node *head = NULL;
+//    TreeNode *root = NULL;
+    FILE *file = fopen("data_generator.txt", "r");
+
     while (!feof(file)) {
+        clock_t start = clock();
+
         Contact *contact = (Contact *) malloc(sizeof(Contact));
         fscanf(file, "%s", contact->firstName);
         fscanf(file, "%s", contact->lastName);
@@ -16,26 +20,35 @@ int main() {
         fscanf(file, "%s", contact->email);
         fscanf(file, "%s", contact->phone);
         fscanf(file, "%s", contact->address);
-        addContactToBinTree(&root, contact);
-//        addContactToList(&head, contact);
+//        addContactToBinTree(&root, contact);
+        addContactToList(&head, contact);
+
+        clock_t end = clock();
+        float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+
+        printf("%.6lf\n",seconds);
+
     }
     fclose(file);
 
-    printf("\nPRINTING ALL TREE\n");
-    printAddressBookBinTree(root);
-    printf("\nSEARCHING ONE ELEMENT\n");
-    printContact(searchContactBinTree(root,root->contact)->contact);
-    printf("\nSORTING BY PHONE\n");
-    sortBinTree(&root,phone);
-    printAddressBookBinTree(root);
-    printf("\nDELETING ROOT\n");
-    deleteContactFromBinTree(&root,root->contact);
-    printf("\nPRINTING ALL TREE (AFTER DELETING ONE ELEMET)\n");
-    printAddressBookBinTree(root);
-    printf("\nDELETING ALL TREE\n");
-    deleteAddressBookBinTree(&root);
-    printf("\nPRINTING ALL TREE (AFTER DELETE)\n");
-    printAddressBookBinTree(root);
+//    printf("\nPRINTING ALL TREE\n");
+//    printAddressBookBinTree(root);
+//    printf("\nSEARCHING ONE ELEMENT\n");
+//    printContact(searchContactBinTree(root,root->contact)->contact);
+//    printf("\nSORTING BY PHONE\n");
+//    sortBinTree(&root,phone);
+//    printAddressBookBinTree(root);
+//    printf("\nDELETING ROOT\n");
+//    deleteContactFromBinTree(&root,root->contact);
+//    printf("\nPRINTING ALL TREE (AFTER DELETING ONE ELEMET)\n");
+//    printAddressBookBinTree(root);
+//    printf("\nDELETING ALL TREE\n");
+//    deleteAddressBookBinTree(&root);
+//    printf("\nPRINTING ALL TREE (AFTER DELETE)\n");
+//    printAddressBookBinTree(root);
+
+
+
 
 //    printf("Size = %d\n", sizeOfAddressBookList(head));
 //    printf("\nPrinting all elements in list\n");
