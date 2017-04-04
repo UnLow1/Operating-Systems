5e3a044d72f36002b54859bafee66160e3d85ca3
Adam Jamka <adam.jamka.1995@gmail.com>
2017-04-04 14:34:57 -0700

    Update main.c

--- Task04/Ex1/main.c
+++ Task04/Ex1/main.c
@@ -1,103 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <wait.h>
-#include <time.h>
+#include <stdlib.h>
+#include <signal.h>
 #include <stdbool.h>
 
-int counter, N, M, copyM;
-pid_t *pids;
+bool forward = true;
 
-void sighandler(int signum, siginfo_t *siginfo, void *context) {
-    if (signum == SIGUSR1) {
-        printf("%d Received SIGUSR1 %d\n", getpid(), signum);
-        if (M > 0) {
-            M--;
-            pids[M] = siginfo->si_pid;
-            printf("M = %d    pid[%d] = %d\n", M, M, pids[M]);
-            if (M == 0) {
-                int i;
-                for (i = 0; i < copyM; i++) {
-                    printf("kill(%d, SIGUSR2)\n", pids[i]);
-                    kill(pids[i], SIGUSR2);
-                }
-            }
-        } else {
-            printf("kill(%d, SIGUSR2)\n", siginfo->si_pid);
-            kill(siginfo->si_pid, SIGUSR2);
-        }
-    } else if (signum == SIGINT) {
-        printf("%d Received SIGINT %d\n", getpid(), signum);
+void sigHandler(int signum)
+{
+    if (signum == SIGINT) {
+        printf("Received signal SIGINT.\n");
         exit(1);
-    } else if (signum == SIGUSR2) {
-        printf("%d Received SIGUSR2 %d\n", getpid(), signum);
-        int random = (unsigned) rand() % SIGRTMAX - SIGRTMIN;
-        random += SIGRTMIN;
-        kill(siginfo->si_pid, SIGUSR2);
-        //losuj
-    } else {
-        printf("%d Received signal %d\n", getpid(), signum);
+    }
+    else if (signum == SIGTSTP) {
+        forward = !forward;
     }
 }
 
+
 int main(int argc, char *argv[]) {
-    counter = 0;
-    pids = malloc(M * sizeof(int));
-    if (argc != 3) {
-        perror("Wrong arguments! Arguments should be N and M");
-        return -1;
+
+    char letter = 'A';
+    struct sigaction sa;
+    sa.sa_handler = sigHandler;
+
+    while(1) {
+        printf(" %c\n",letter);
+        usleep(150000);
+
+        if (forward)
+            letter++;
+        else
+            letter--;
+
+        if(letter > 'Z')
+            letter = 'A';
+        else if (letter < 'A')
+            letter = 'Z';
+
+        signal(SIGINT, sigHandler);
+        sigaction(SIGTSTP, &sa, NULL);
     }
-    N = atoi(argv[1]);
-    M = atoi(argv[2]);
-    copyM = M;
-    time_t t;
-    srand((unsigned) time(&t));
-    int parent_pid = getpid();
-    struct sigaction act;
-    act.sa_sigaction = &sighandler;
-    act.sa_flags = SA_SIGINFO;
-    sigaction(SIGINT, &act, NULL);
-    sigaction(SIGUSR1, &act, NULL);
-    sigaction(SIGUSR2, &act, NULL);
-    int i;
-    for (i = SIGRTMIN; i <= SIGRTMAX; i++)
-        sigaction(i, &act, NULL);
-    printf("%d\n", counter);
-
-    for (i = 0; i < N; ++i) {
-        unsigned int r = (unsigned) rand() % 11;
-        if (fork() == 0) {
-            // do the job specific to the child process
-            int child_pid;
-            child_pid = getpid();
-            printf("%d Start working\n", child_pid);
-            printf("%d Going sleep for %d seconds\n", child_pid, r);
-            union sigval value;
-            value.sival_int = i;
-            sleep(r);
-            if (sigqueue(parent_pid, SIGUSR1, value) != 0) {
-                perror("SIGSENT-ERROR:");
-                exit(1);
-            }
-            while (true) {
-                sleep(1);
-            }   // just wait
-
-            printf("%d End working\n", child_pid);
-
-            // don't forget to exit from the child
-            exit(0);
-        }
-
-    }
-
-
-
-// wait all child processes
-    int status;
-    for (i = 0; i < N; ++i)
-        wait(&status);
-    printf("END MATHER %d\n", parent_pid);
 
     return 0;
 }
