47eeff01515fdd52d3d107ef96664c589746647f
Adam Jamka <adam.jamka.1995@gmail.com>
2017-04-05 06:58:12 -0700

    Update main.c

--- Task04/Ex2/main.c
+++ Task04/Ex2/main.c
@@ -4,57 +4,72 @@
 #include <wait.h>
 #include <time.h>
 #include <stdbool.h>
+#include <sys/resource.h>
 
-int counter, N, M, copyM;
+int N, M, ind;
 pid_t *pids;
 pid_t parent_pid;
 
+void timeCheckpoint(const char *message) {
+    static struct timespec previous_real_time;
+    // Getting time values
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    struct timespec real_time;
+    clock_gettime(CLOCK_REALTIME, &real_time);
+    // Show info
+    printf("real time: %lld  ---  %s\n",
+           (long long)(real_time.tv_sec-previous_real_time.tv_sec)*1000000+(real_time.tv_nsec-previous_real_time.tv_nsec)/1000,
+           message);
+    previous_real_time = real_time;
+}
+
 void sighandler(int signum, siginfo_t *siginfo, void *context) {
     if (signum == SIGUSR1) {
-        printf("%d Received SIGUSR1 %d\n", getpid(), signum);
+        printf("%d Received SIGUSR1 From %d\n", getpid(), siginfo->si_pid);
+        pids[ind] = siginfo->si_pid;
+        ind++;
+        timeCheckpoint("SIGUSR! SENT");
         if (M > 0) {
             M--;
-            pids[M] = siginfo->si_pid;
-            printf("M = %d    pid[%d] = %d\n", M, M, pids[M]);
             if (M == 0) {
                 int i;
-                for (i = 0; i < copyM; i++) {
-                    printf("kill(%d, SIGUSR2)\n", pids[i]);
+                for (i = 0; i < ind; i++) {
+//                    printf("kill(%d, SIGUSR2)\n", pids[i]);
                     kill(pids[i], SIGUSR2);
                 }
             }
         } else {
-            printf("kill(%d, SIGUSR2)\n", siginfo->si_pid);
+//            printf("kill(%d, SIGUSR2)\n", siginfo->si_pid);
             kill(siginfo->si_pid, SIGUSR2);
         }
     } else if (signum == SIGINT) {
-        printf("%d Received SIGINT %d\n", getpid(), signum);
+        printf("%d Received SIGINT From %d\n", getpid(), siginfo->si_pid);
         exit(1);
     } else if (signum == SIGUSR2) {
-        printf("%d Received SIGUSR2 %d\n", getpid(), signum);
+        printf("%d Received SIGUSR2 From %d\n", getpid(), siginfo->si_pid);
         int random = (unsigned) rand() % (SIGRTMAX - SIGRTMIN);
         random += SIGRTMIN;
-        printf("kill(%d, random)\n", siginfo->si_pid);
+//        printf("kill(%d, %d)\n", siginfo->si_pid,random);
         kill(siginfo->si_pid, random);
-        //losuj
     } else {
-        printf("%d Received signal %d\n", getpid(), signum);
+        printf("%d Received signal %d From %d\n", getpid(), signum, siginfo->si_pid);
     }
 }
 
 int main(int argc, char *argv[]) {
-    counter = 0;
     pids = malloc(M * sizeof(int));
     if (argc != 3) {
         perror("Wrong arguments! Arguments should be N and M");
-        return -1;
+        exit(1);
     }
     N = atoi(argv[1]);
     M = atoi(argv[2]);
-    copyM = M;
+    ind = 0;
     time_t t;
     srand((unsigned) time(&t));
     parent_pid = getpid();
+    printf("PARENT PID = %d\n",parent_pid);
     struct sigaction act;
     act.sa_sigaction = &sighandler;
     act.sa_flags = SA_SIGINFO;
@@ -64,7 +79,6 @@ int main(int argc, char *argv[]) {
     int i;
     for (i = SIGRTMIN; i <= SIGRTMAX; i++)
         sigaction(i, &act, NULL);
-    printf("%d\n", counter);
 
     for (i = 0; i < N; ++i) {
         unsigned int r = (unsigned) rand() % 11;
@@ -74,10 +88,8 @@ int main(int argc, char *argv[]) {
             child_pid = getpid();
             printf("%d Start working\n", child_pid);
             printf("%d Going sleep for %d seconds\n", child_pid, r);
-            union sigval value;
-            value.sival_int = i;
             sleep(r);
-            if (sigqueue(parent_pid, SIGUSR1, value) != 0) {
+            if (kill(parent_pid, SIGUSR1) != 0) {
                 perror("SIGSENT-ERROR:");
                 exit(1);
             }
@@ -92,14 +104,16 @@ int main(int argc, char *argv[]) {
         }
 
     }
-
-
-
 // wait all child processes
     int status;
-    for (i = 0; i < N; ++i)
-        wait(&status);
-    printf("END MATHER %d\n", parent_pid);
+    for (i = 0; i < N; i++) {
+        waitpid(pids[i], &status, 0);
+        status = WEXITSTATUS(status);
+        printf("%d exited with code %d\n", pids[i], status);
+    }
+
+    sleep(1);
+    printf("END PARENT %d\n", parent_pid);
 
     return 0;
 }
