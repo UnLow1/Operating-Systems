2f05d0addb8f8ccfdf8793a4024b08fd3056275a
Adam Jamka <adam.jamka.1995@gmail.com>
2017-05-15 14:26:21 -0700

    Update barber.c

--- Task07/Ex01/barber.c
+++ Task07/Ex01/barber.c
@@ -6,9 +6,16 @@
 #include <signal.h>
 #include <stdbool.h>
 #include <sys/shm.h>
+#include <unistd.h>
 #include "header.h"
 
 void timeCheckpoint();
+
+void sighandler(int signum);
+
+void semaphore(unsigned short num, short op, short flg, struct sembuf *buffer, int semID);
+
+bool open;
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -22,80 +29,64 @@ int main(int argc, char *argv[]) {
     int queueID = shmget(key, sizeof(struct MyQueue), IPC_CREAT | 0666);
     struct MyQueue *myQueue = (struct MyQueue *) shmat(queueID, 0, 0);
 
-    bool open = true;
+    myQueue->max_size = N;
+    myQueue->chair = 0;
+    myQueue->actual_size = 0;
+    open = true;
+
     // createSemaphores
     int semID = semget(key, 2, IPC_CREAT | 0666);
     semctl(semID, 0, SETVAL, 0);
     semctl(semID, 1, SETVAL, 1);
 
     printf("Barber in work\n");
-    struct sembuf *buffer;
+    struct sembuf buffer;
 
     while (open) {
         timeCheckpoint();
         printf("Barber is going sleep\n");
-        // takeSemaphore
-        buffer->sem_num = 1;       // FIFO == 1
-        buffer->sem_op = -1;
-        buffer->sem_flg = 0;
-        semop(semID, buffer, 1);
+
+        semaphore(0, -1, 0, &buffer, semID);
 
         pid_t clientID;
-        // takeSemaphore
-        buffer->sem_num = 1;       // FIFO == 1
-        buffer->sem_op = -1;
-        buffer->sem_flg = 0;
-        semop(semID, buffer, 1);
+
+        semaphore(1, -1, 0, &buffer, semID);
 
         clientID = myQueue->chair;
-        // giveSemaphore
-        buffer->sem_num = 1;    // FIFO == 1
-        buffer->sem_op = 1;
-        buffer->sem_flg = 0;
-        semop(semID, buffer, 1);
+
+        semaphore(1, 1, 0, &buffer, semID);
 
         timeCheckpoint();
-        printf("Barber is cutting client %d\n", clientID);
+        printf("Barber start cutting client %d\n", clientID);
         kill(clientID, SIGUSR1);
         timeCheckpoint();
         printf("Barber end cutting client %d\n", clientID);
 
         while (true) {
-            //takeSemaphore
-            buffer->sem_num = 1;       // FIFO == 1
-            buffer->sem_op = -1;
-            buffer->sem_flg = 0;
-            semop(semID, buffer, 1);
+
+            semaphore(1, -1, 0, &buffer, semID);
 
             if (myQueue->actual_size > 0) {
                 clientID = myQueue->queue[0];
                 int i;
-                for (i = 0; i < myQueue->actual_size; ++i) {
+                for (i = 0; i < myQueue->actual_size - 1; ++i) {
                     myQueue->queue[i] = myQueue->queue[i + 1];
                 }
-                // giveSemaphore
-                buffer->sem_num = 1;    // FIFO == 1
-                buffer->sem_op = 1;
-                buffer->sem_flg = 0;
-                semop(semID, buffer, 1);
+                myQueue->actual_size--;
+
+                semaphore(1, 1, 0, &buffer, semID);
 
                 timeCheckpoint();
-                printf("Barber is cutting client %d\n", clientID);
+                printf("Barber start cutting client %d\n", clientID);
+                sleep(1);
                 kill(clientID, SIGUSR1);
                 timeCheckpoint();
                 printf("Barber end cutting client %d\n", clientID);
             } else {
-                // takeSemaphore
-                buffer->sem_num = 0;       // barber == 0
-                buffer->sem_op = -1;
-                buffer->sem_flg = 0;
-                semop(semID, buffer, 1);
 
-                // giveSemaphore
-                buffer->sem_num = 1;    // FIFO == 1
-                buffer->sem_op = 1;
-                buffer->sem_flg = 0;
-                semop(semID, buffer, 1);
+                semaphore(0, -1, 0, &buffer, semID);
+
+                semaphore(1, 1, 0, &buffer, semID);
 
                 break;
             }
@@ -109,5 +100,20 @@ int main(int argc, char *argv[]) {
 void timeCheckpoint() {
     struct timespec real_time;
     clock_gettime(CLOCK_MONOTONIC, &real_time);
-    printf("timeCheckpoint %lld\n", (long long) (real_time.tv_sec * 1000000 + real_time.tv_nsec / 1000));
+    printf("timeCheckpoint %lld    ", (long long) (real_time.tv_sec * 1000000 + real_time.tv_nsec / 1000));
+}
+
+void sighandler(int signum) {
+    if (signum == SIGINT) {
+        printf("\nBarber, interrupted\n");
+        open = false;
+        exit(1);
+    }
+}
+
+void semaphore(unsigned short num, short op, short flg, struct sembuf *buffer, int semID) {
+    buffer->sem_num = num;
+    buffer->sem_op = op;
+    buffer->sem_flg = flg;
+    semop(semID, buffer, 1);
 }
